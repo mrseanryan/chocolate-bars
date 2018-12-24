10a3b249c39a87900fb9384c6efbe3eed95cb171
mrseanryan <str_ie@yahoo.co.uk>
2018-12-24 12:51:30 +0000

    (chore) Fix tslint again.

--- src/chocolate-bars.ts
+++ src/chocolate-bars.ts
@@ -1,6 +1,7 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+//tslint-disable:no-unnecessary-class
 
-}
+// TODO xxx remove
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+export default class DummyClass {}
