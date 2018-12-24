13dbd813e3c27af34f1c21aad3895e8c367cca6a
mrseanryan <str_ie@yahoo.co.uk>
2018-12-24 11:37:58 +0000

    (chore) Add test images

---
+++ testUtils/TestImages.ts
@@ -0,0 +1,10 @@
+export namespace TestImages {
+    export const PATH_TO_TEST_DATA = "./static/testData";
+
+    export const PATH_TO_TEST_DATA_GOOD = PATH_TO_TEST_DATA + "/goodQuality";
+
+    export const PATH_TO_TEST_DATA_BAD = PATH_TO_TEST_DATA + "/badQuality";
+
+    export const PATH_TO_BLURRED_IMAGE = PATH_TO_TEST_DATA_BAD + "/out_of_Focus_1.jpg";
+    export const PATH_TO_GOOD_IMAGE = PATH_TO_TEST_DATA_GOOD + "/flower-631765_960_720.jpg";
+}
