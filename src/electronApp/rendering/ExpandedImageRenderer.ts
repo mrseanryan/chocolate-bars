0e978e61564cc282e89ecd94459fea4e35949f1c
str <str_ie@yahoo.co.uk>
2018-12-29 14:32:27 +0100

    refactor: Extract CSS class name in ExpandedImageRenderer.

--- src/electronApp/rendering/ExpandedImageRenderer.ts
+++ src/electronApp/rendering/ExpandedImageRenderer.ts
@@ -3,9 +3,11 @@ import * as jquery from "jquery";
 import { ImageDetail } from "../../bars/model/ImageDetail";
 import { JQueryUtils } from "../../utils/JQueryUtils";
 
+const SHOW_POPUP_BUTTON_CLASS = "user-image-popup";
+
 export namespace ExpandedImageRenderer {
     export function renderHiddenPopup() {
-        const html = `<div class="user-image-popup" />`;
+        const html = `<div class="${SHOW_POPUP_BUTTON_CLASS}" />`;
 
         jquery("body").append(html);
 
@@ -13,23 +15,23 @@ export namespace ExpandedImageRenderer {
     }
 
     function addClickExpandedImageListener() {
-        jquery(".user-image-popup").on("click", () => {
+        jquery(`.${SHOW_POPUP_BUTTON_CLASS}`).on("click", () => {
             onClickExpandedImagePopup();
         });
     }
 
     export function onClickExpandImage(image: ImageDetail) {
-        JQueryUtils.clearHtmlDivByClass("user-image-popup");
+        JQueryUtils.clearHtmlDivByClass(SHOW_POPUP_BUTTON_CLASS);
 
         // use the smaller image, as is smaller and already loaded - so faster
         const imageHtml = `<img src="${image.smallerFilepath}" />`;
 
-        jquery(".user-image-popup").append(imageHtml);
+        jquery(`.${SHOW_POPUP_BUTTON_CLASS}`).append(imageHtml);
 
-        jquery(".user-image-popup").addClass("user-image-popup-visible");
+        jquery(`.${SHOW_POPUP_BUTTON_CLASS}`).addClass("user-image-popup-visible");
     }
 
     export function onClickExpandedImagePopup() {
-        jquery(".user-image-popup").removeClass("user-image-popup-visible");
+        jquery(`.${SHOW_POPUP_BUTTON_CLASS}`).removeClass("user-image-popup-visible");
     }
 }
