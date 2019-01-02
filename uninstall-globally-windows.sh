7c62e06a7869a1edf8d2c6f9b208f7b2c332583f
str <str_ie@yahoo.co.uk>
2019-01-02 11:16:19 +0100

    build: Add script to uninstall globally on Windows

---
+++ uninstall-globally-windows.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+NODEJS_GLOBAL=/c/Program\ Files/nodejs
+
+echo Removing chocolate-bars from $NODEJS_GLOBAL ...
+
+rm -rf /c/Program\ Files/nodejs/node_modules/chocolate-bars/
+
+rm /c/Program\ Files/nodejs/chocolate-bars*
