a900aa8903844b7d6a69770e030c705d9dc42595
str <str_ie@yahoo.co.uk>
2020-01-17 12:35:37 +0100

    feat: Show confirmation after move or copy. fixes #12.

--- src/electronApp/rendering/MovedStarredImagesRenderer.ts
+++ src/electronApp/rendering/MovedStarredImagesRenderer.ts
@@ -1,7 +1,8 @@
 import { DirectorySelectorDialog } from "../../utils/DirectorySelectorDialog";
-import { ImageMover, MoveOrCopy } from "../../utils/ImageMover";
+import { ImageMover, MoveOrCopy, MoveOrCopyUtils } from "../../utils/ImageMover";
 import { IOutputter } from "../../utils/outputter/IOutputter";
 import { State } from "../State";
+import { ConfirmationDialog } from "./ConfirmationDialog";
 
 type ButtonConfig = {
     mode: MoveOrCopy;
@@ -67,32 +68,83 @@ export namespace MoveStarredImagesRenderer {
         }
 
         button.addEventListener("click", _ => {
-            const directories = DirectorySelectorDialog.selectImagesDirectory(
-                buttonConfig.dialog.title,
-                buttonConfig.dialog.buttonText
+            ImageMover.hasStarredImagesThisPage(state.imageInputDir, state, outputter).then(
+                hasStars => {
+                    if (!hasStars) {
+                        showConfirmation(
+                            buttonConfig.mode,
+                            "Please first add stars to some images.",
+                            () => {
+                                return;
+                            }
+                        );
+                        return;
+                    } else {
+                        handleMoveOrCopyImages(
+                            state,
+                            renderImagesAndPagerForDirectorySamePage,
+                            buttonConfig,
+                            outputter
+                        );
+                    }
+                }
             );
-
-            const imageInputDir = state.imageInputDir;
-
-            if (directories && directories.length === 1) {
-                ImageMover.moveStarredImagesTo(
-                    imageInputDir,
-                    directories[0],
-                    buttonConfig.mode,
-                    state,
-                    outputter
-                )
-                    .then(() => {
-                        // refresh the current directory:
-                        renderImagesAndPagerForDirectorySamePage(imageInputDir);
-                    })
-                    .catch((err: any) => {
-                        console.error(err);
-
-                        // some files may have moved OK - so refresh the current directory:
-                        renderImagesAndPagerForDirectorySamePage(imageInputDir);
-                    });
-            }
         });
+    }
+
+    function showConfirmation(mode: MoveOrCopy, message: string, action: () => void) {
+        ConfirmationDialog.show(
+            `${MoveOrCopyUtils.toTextCapitalized(mode)} images`,
+            message,
+            action
+        );
+    }
+
+    function handleMoveOrCopyImages(
+        state: State,
+        renderImagesAndPagerForDirectorySamePage: (imageInputDir: string) => void,
+        buttonConfig: ButtonConfig,
+        outputter: IOutputter
+    ) {
+        const directories = DirectorySelectorDialog.selectImagesDirectory(
+            buttonConfig.dialog.title,
+            buttonConfig.dialog.buttonText
+        );
+
+        const imageInputDir = state.imageInputDir;
+
+        if (directories && directories.length === 1) {
+            ImageMover.moveStarredImagesTo(
+                imageInputDir,
+                directories[0],
+                buttonConfig.mode,
+                state,
+                outputter
+            )
+                .then(() => {
+                    showConfirmation(
+                        buttonConfig.mode,
+                        `Images were ${MoveOrCopyUtils.toTextPastTense(buttonConfig.mode)}.`,
+                        () => {
+                            // refresh the current directory:
+                            renderImagesAndPagerForDirectorySamePage(imageInputDir);
+                        }
+                    );
+                })
+                .catch((err: any) => {
+                    console.error(err);
+
+                    showConfirmation(
+                        buttonConfig.mode,
+                        `Some images could not be ${MoveOrCopyUtils.toTextPastTense(
+                            buttonConfig.mode
+                        )}. ${err}`,
+                        () => {
+                            // some files may have moved OK - so refresh the current directory:
+                            renderImagesAndPagerForDirectorySamePage(imageInputDir);
+                        }
+                    );
+                });
+        }
     }
 }
