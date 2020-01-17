20b1cfaca7e0acce9d61772f255266c9a9674d4e
str <str_ie@yahoo.co.uk>
2020-01-17 14:33:48 +0100

    chore: Rename a test folder. Restore exif test images.

---
+++ testOnlyOneLarge.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yarn build:minimal && ./go.sh ./static/testData/onlyOneLarge
--- testOnlyOneTooBig.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-yarn build:minimal && ./go.sh ./static/testData/onlyOneTooBig
